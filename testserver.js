66db082896662243f326dab6149abd3c0331e3f0
prageeeeth <67830194+prageeeeth@users.noreply.github.com>
2022-10-15 09:02:37 +0530

    Create testserver.js

---
+++ testserver.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const server = app.listen(7000, () => {
+  console.log(`Express running ? PORT ${server.address().port}`);
+});
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
